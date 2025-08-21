a943e5e983d81b2952632f0adabc77d8a84af10a
Muhammad Shayan Zahid <shayanzahid0@gmail.com>
2025-08-22 01:45:42 +0500

    Initial Commit

---
+++ PDFChat/PDFChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFChatApp.swift
+//  PDFChat
+//
+//  Created by Muhammad Shayan Zahid on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PDFChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
